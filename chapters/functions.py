9ffd3ec4534021e37c12f2e0f4a1a9d096095a78
Krishna Ghodke <krushnaghodke90@gmail.com>
2022-02-26 18:55:10 -0800

    function changes

--- chapters/functions.py
+++ chapters/functions.py
@@ -1,7 +1,9 @@
 def days_to_unit(days: int, unit: str):
     min_unit = days * 24 * 60
     print(f"{days} days have {min_unit} {unit}")
-    print("All Good!")
 
 
 days_to_unit(25, "min")
+days_to_unit(35, "min")
+days_to_unit(45, "min")
+days_to_unit(55, "min")
